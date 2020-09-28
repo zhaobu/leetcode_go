64ba5cb511855edef8ec35b8f7153517c323e23f
lwxm <lwxm>
2020-09-28 22:13:09 +0800

    1:优化Rotate

--- src/0189.Rotate-Array/solution.go
+++ src/0189.Rotate-Array/solution.go
@@ -1,8 +1,6 @@
 package Solution
 
-/*
-[小浩算法](https://www.geekxh.com/1.0.数组系列/004.html#_03、题目解答)
-*/
+// [小浩算法](https://www.geekxh.com/1.0.数组系列/004.html#_03、题目解答)
 func Rotate1(nums []int, k int) {
 	reverse(nums)
 	reverse(nums[0 : k%len(nums)])
