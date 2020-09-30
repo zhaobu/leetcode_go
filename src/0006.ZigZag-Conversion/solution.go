8dd88ee7073cce884c242b2cd63b5f9401342336
lwcywin10 <lwcywin10>
2020-09-30 19:18:25 +0800

    1:t添加6. Z 字形变换

--- src/0006.ZigZag-Conversion/solution.go
+++ src/0006.ZigZag-Conversion/solution.go
@@ -1,6 +1,10 @@
 package Solution
 
 func Convert1(s string, numRows int) string {
+	records := make([][]byte, 0, numRows)
 	res := ""
+	for i, j := 0, 0; j < len(s); i = (i + 1) % numRows {
+
+	}
 	return res
 }
