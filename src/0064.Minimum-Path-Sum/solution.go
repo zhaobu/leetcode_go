eaf70d2170638f9904345d1d2f0a1cd5a6e936f4
lwcywin10 <lwcywin10>
2020-10-12 18:52:49 +0800

    添加64. 最小路径和

---
+++ src/0064.Minimum-Path-Sum/solution.go
@@ -0,0 +1,18 @@
+package Solution
+
+/*
+[小浩算法 ：动态规划)](https://www.geekxh.com/1.2.%E5%8A%A8%E6%80%81%E8%A7%84%E5%88%92%E7%B3%BB%E5%88%97/204.html#_03%E3%80%81go%E8%AF%AD%E8%A8%80%E7%A4%BA%E4%BE%8B)
+*/
+func MinPathSum1(grid [][]int) int {
+	for i := 0; i < count; i++ {
+
+	}
+	return 0
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
--- src/0120.Triangle/README.md
+++ src/0120.Triangle/README.md
@@ -27,7 +27,7 @@
 
 ## 题解
 
-1. [windliang](https://leetcode.wang/leetCode-6-ZigZag-Conversion.html)
+1. [windliang](https://leetcode.wang/leetcode-120-Triangle.html)
 2. [leetcode 官方](https://leetcode-cn.com/problems/zigzag-conversion/solution/z-zi-xing-bian-huan-by-leetcode/)
 3. [小浩算法](https://www.geekxh.com/1.0.数组系列/009.html#_02、题目分析)
 
