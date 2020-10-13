fced1d612fd0974a91980c01ac8d270de2294727
lwcywin10 <lwcywin10>
2020-10-13 14:34:43 +0800

    添加28. 实现 strStr()

---
+++ src/0028.Implement-strStr/solution.go
@@ -0,0 +1,9 @@
+package Solution
+
+/*
+[小浩算法  使用hash)](https://www.geekxh.com/1.3.%E5%AD%97%E7%AC%A6%E4%B8%B2%E7%B3%BB%E5%88%97/302.html#_02%E3%80%81%E9%A2%98%E7%9B%AE%E5%9B%BE%E8%A7%A3)
+*/
+func StrStr1(haystack string, needle string) int {
+
+	return 0
+}
---
+++ src/0028.Implement-strStr/solution_test.go
@@ -0,0 +1,42 @@
+package Solution
+
+import (
+	"testing"
+)
+
+// func init() {
+// 	zaplog.InitLog(&zaplog.Config{
+// 		Logdir:   "./",
+// 		LogName:  "main.log",
+// 		Loglevel: "debug",
+// 		Debug:    true},
+// 	)
+// }
+
+//定义结构
+type Input struct {
+	haystack string
+	needle   string
+}
+
+type Case struct {
+	name   string
+	input  *Input
+	expect int
+}
+
+func TestStrStr1(t *testing.T) {
+	//	测试用例
+	cases := []*Case{
+		{name: "test 1", input: &Input{haystack: "hello", needle: "ll"}, expect: 2},
+		{name: "test 2", input: &Input{haystack: "aaaaa", needle: "bba"}, expect: -1},
+	}
+
+	//	开始测试
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			out := StrStr1(c.input.haystack, c.input.needle)
+			t.Logf("success expect: %v, output: %v, with input: %+v", c.expect, out, c.input)
+		})
+	}
+}
