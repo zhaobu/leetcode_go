8b4343e8fcc61998bb3d3a55bcf3bb743d5df345
lwcywin10 <lwcywin10>
2020-10-13 10:06:47 +0800

    优化64. 最小路径和

--- src/0064.Minimum-Path-Sum/solution.go
+++ src/0064.Minimum-Path-Sum/solution.go
@@ -1,32 +1,28 @@
 package Solution
 
 /*
-[小浩算法 ：动态规划)](https://www.geekxh.com/1.2.%E5%8A%A8%E6%80%81%E8%A7%84%E5%88%92%E7%B3%BB%E5%88%97/204.html#_03%E3%80%81go%E8%AF%AD%E8%A8%80%E7%A4%BA%E4%BE%8B)
+[小浩算法 ：动态规划)](https://www.geekxh.com/1.2.%E5%8A%A8%E6%80%81%E8%A7%84%E5%88%92%E7%B3%BB%E5%88%97/205.html#_02%E3%80%81%E9%A2%98%E7%9B%AE%E5%9B%BE%E8%A7%A3)
 */
 func MinPathSum1(grid [][]int) int {
-	for i := 0; i < len(grid); i++ {
-		for j := 0; j < len(grid[i]); j++ {
-			if i == 0 && j == 0 {
-				continue
-			}
-			if j == 0 {
-				grid[i][j] += grid[i-1][j]
-				continue
-			}
-			if i == 0 {
-				grid[i][j] += grid[i][j-1]
-				continue
-			}
-			grid[i][j] = min(grid[i-1][j], grid[i][j-1]) + grid[i][j]
-		}
-	}
 	m := len(grid)
 	n := len(grid[m-1])
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			if i != 0 && j != 0 {
+				grid[i][j] = min(grid[i-1][j], grid[i][j-1]) + grid[i][j]
+			} else if j == 0 && i != 0 {
+				grid[i][j] += grid[i-1][j]
+			} else if i == 0 && j != 0 {
+				grid[i][j] += grid[i][j-1]
+			}
+		}
+	}
 	return grid[m-1][n-1]
 }
 
 func min(a, b int) int {
 	if a < b {
+
 		return a
 	}
 	return b
