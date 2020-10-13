b3916611aaacced5190ec44372088723ca7846ba
lwxm <lwxm>
2020-10-13 19:48:48 +0800

    完成28. 实现 strStr()的暴力模式

--- src/0028.Implement-strStr/solution.go
+++ src/0028.Implement-strStr/solution.go
@@ -1,9 +1,38 @@
 package Solution
 
 /*
-[小浩算法  使用hash)](https://www.geekxh.com/1.3.%E5%AD%97%E7%AC%A6%E4%B8%B2%E7%B3%BB%E5%88%97/302.html#_02%E3%80%81%E9%A2%98%E7%9B%AE%E5%9B%BE%E8%A7%A3)
+[暴力模式](https://www.geekxh.com/1.3.%E5%AD%97%E7%AC%A6%E4%B8%B2%E7%B3%BB%E5%88%97/303.html#_02%E3%80%81sunday-%E5%8C%B9%E9%85%8D)
 */
 func StrStr1(haystack string, needle string) int {
+	if needle == "" {
+		return 0
+	}
+	if len(haystack) < len(needle) {
+		return -1
+	}
+	l1 := len(haystack)
+	l2 := len(needle)
+	for i := 0; i <= l1-l2; i++ {
+		for j := 0; j < l2; j++ {
+			if haystack[i+j] != needle[j] {
+				break
+			}
+			if j == l2-1 {
+				return i
+			}
+		}
+	}
 
-	return 0
+	return -1
+}
+
+func StrStr2(haystack string, needle string) int {
+	if needle == "" {
+		return 0
+	}
+	if len(haystack) < len(needle) {
+		return -1
+	}
+
+	return -1
 }
