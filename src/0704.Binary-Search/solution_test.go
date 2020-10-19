0ff8f7b0281bbe5ea9d0a99d7754dc84217af9f4
lwxm <lwxm>
2020-10-19 18:58:37 +0800

    完成704. 二分查找,并且总结二分法查找左右边界的不同写法

--- src/0704.Binary-Search/solution_test.go
+++ src/0704.Binary-Search/solution_test.go
@@ -87,3 +87,83 @@ func TestSearch3(t *testing.T) {
 		})
 	}
 }
+
+func TestLeftBound1(t *testing.T) {
+	//	测试用例
+	cases := []*Case{
+		{name: "test 1", input: &Input{nums: []int{1, 1, 2, 2, 4, 4, 5, 5}, target: 0}, expect: -1},
+		{name: "test 2", input: &Input{nums: []int{1, 1, 2, 2, 4, 4, 5, 5}, target: 1}, expect: 0},
+		{name: "test 3", input: &Input{nums: []int{1, 1, 2, 2, 4, 4, 5, 5}, target: 4}, expect: 4},
+		{name: "test 4", input: &Input{nums: []int{1, 1, 2, 2, 4, 4, 5, 5}, target: 3}, expect: -1},
+		{name: "test 5", input: &Input{nums: []int{1, 1, 2, 2, 4, 4, 5, 5}, target: 5}, expect: 6},
+		{name: "test 6", input: &Input{nums: []int{1, 1, 2, 2, 4, 4, 5, 5}, target: 6}, expect: -1},
+	}
+
+	//	开始测试
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			out := LeftBound1(c.input.nums, c.input.target)
+			t.Logf("case %s success expect: %v, output: %v, with input: %+v", c.name, c.expect, out, c.input)
+		})
+	}
+}
+
+func TestLeftBound2(t *testing.T) {
+	//	测试用例
+	cases := []*Case{
+		{name: "test 1", input: &Input{nums: []int{1, 1, 2, 2, 4, 4, 5, 5}, target: 0}, expect: -1},
+		{name: "test 2", input: &Input{nums: []int{1, 1, 2, 2, 4, 4, 5, 5}, target: 1}, expect: 0},
+		{name: "test 3", input: &Input{nums: []int{1, 1, 2, 2, 4, 4, 5, 5}, target: 4}, expect: 4},
+		{name: "test 4", input: &Input{nums: []int{1, 1, 2, 2, 4, 4, 5, 5}, target: 3}, expect: -1},
+		{name: "test 5", input: &Input{nums: []int{1, 1, 2, 2, 4, 4, 5, 5}, target: 5}, expect: 6},
+		{name: "test 6", input: &Input{nums: []int{1, 1, 2, 2, 4, 4, 5, 5}, target: 6}, expect: -1},
+	}
+
+	//	开始测试
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			out := LeftBound2(c.input.nums, c.input.target)
+			t.Logf("case %s success expect: %v, output: %v, with input: %+v", c.name, c.expect, out, c.input)
+		})
+	}
+}
+
+func TestRightBound1(t *testing.T) {
+	//	测试用例
+	cases := []*Case{
+		{name: "test 1", input: &Input{nums: []int{1, 1, 2, 2, 4, 4, 5, 5}, target: 0}, expect: -1},
+		{name: "test 2", input: &Input{nums: []int{1, 1, 2, 2, 4, 4, 5, 5}, target: 1}, expect: 1},
+		{name: "test 3", input: &Input{nums: []int{1, 1, 2, 2, 4, 4, 5, 5}, target: 4}, expect: 5},
+		{name: "test 4", input: &Input{nums: []int{1, 1, 2, 2, 4, 4, 5, 5}, target: 3}, expect: -1},
+		{name: "test 5", input: &Input{nums: []int{1, 1, 2, 2, 4, 4, 5, 5}, target: 5}, expect: 7},
+		{name: "test 6", input: &Input{nums: []int{1, 1, 2, 2, 4, 4, 5, 5}, target: 6}, expect: -1},
+	}
+
+	//	开始测试
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			out := RightBound1(c.input.nums, c.input.target)
+			t.Logf("case %s success expect: %v, output: %v, with input: %+v", c.name, c.expect, out, c.input)
+		})
+	}
+}
+
+func TestRightBound2(t *testing.T) {
+	//	测试用例
+	cases := []*Case{
+		{name: "test 1", input: &Input{nums: []int{1, 1, 2, 2, 4, 4, 5, 5}, target: 0}, expect: -1},
+		{name: "test 2", input: &Input{nums: []int{1, 1, 2, 2, 4, 4, 5, 5}, target: 1}, expect: 1},
+		{name: "test 3", input: &Input{nums: []int{1, 1, 2, 2, 4, 4, 5, 5}, target: 4}, expect: 5},
+		{name: "test 4", input: &Input{nums: []int{1, 1, 2, 2, 4, 4, 5, 5}, target: 3}, expect: -1},
+		{name: "test 5", input: &Input{nums: []int{1, 1, 2, 2, 4, 4, 5, 5}, target: 5}, expect: 7},
+		{name: "test 6", input: &Input{nums: []int{1, 1, 2, 2, 4, 4, 5, 5}, target: 6}, expect: -1},
+	}
+
+	//	开始测试
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			out := RightBound2(c.input.nums, c.input.target)
+			t.Logf("case %s success expect: %v, output: %v, with input: %+v", c.name, c.expect, out, c.input)
+		})
+	}
+}
