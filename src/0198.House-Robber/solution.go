0d31a93403ec0816dfc53002ca838933a39ba831
lwxm <lwxm>
2020-11-07 19:15:19 +0800

    198. 打家劫舍添加自底向上动态规划

--- src/0198.House-Robber/solution.go
+++ src/0198.House-Robber/solution.go
@@ -24,3 +24,42 @@ func max(a, b int) int {
 	}
 	return a
 }
+
+/*
+[labuladong 经典动态规划：打家劫舍系列问题 (带备忘录的自顶向下动态规划)](https://mp.weixin.qq.com/s?__biz=MzAxODQxMDM0Mw==&mid=2247484800&idx=1&sn=1016975b9e8df0b8f6df996a5fded0af&chksm=9bd7fb88aca0729eb2d450cca8111abd8f861236b04125ce556171cb520e298ddec4d90823b3&scene=21#wechat_redirect)
+*/
+func Rob2(nums []int) int {
+	if len(nums) < 1 {
+		return 0
+	}
+	dp := make(map[int]int, len(nums))
+	return _rob2(dp, nums, 0)
+}
+
+func _rob2(dp map[int]int, nums []int, start int) int {
+	if start >= len(nums) {
+		return 0
+	}
+	if v, ok := dp[start]; ok {
+		return v
+	}
+	res := max(_rob2(dp, nums, start+1), nums[start]+_rob2(dp, nums, start+2))
+	dp[start] = res
+	return res
+}
+
+/*
+[labuladong 经典动态规划：打家劫舍系列问题 (自底向上动态规划)](https://mp.weixin.qq.com/s?__biz=MzAxODQxMDM0Mw==&mid=2247484800&idx=1&sn=1016975b9e8df0b8f6df996a5fded0af&chksm=9bd7fb88aca0729eb2d450cca8111abd8f861236b04125ce556171cb520e298ddec4d90823b3&scene=21#wechat_redirect)
+*/
+func Rob3(nums []int) int {
+	if len(nums) < 1 {
+		return 0
+	}
+	dp1, dp2, dpi := 0, 0, 0 //分别记录dp[i-1],dp[i-2],dp[i]
+	for i := 0; i < len(nums); i++ {
+		dpi = max(dp1, dp2+nums[i]) //分不偷nums[i]和偷nums[i]两种情况
+		dp2 = dp1
+		dp1 = dpi
+	}
+	return dpi
+}
--- src/0198.House-Robber/solution_test.go
+++ src/0198.House-Robber/solution_test.go
@@ -39,3 +39,36 @@ func TestRob1(t *testing.T) {
 		})
 	}
 }
+
+func TestRob2(t *testing.T) {
+	//	测试用例
+	cases := []*Case{
+		{name: "test 1", input: &Input{nums: []int{1, 2, 3, 1}}, expect: 4},
+		{name: "test 2", input: &Input{nums: []int{2, 7, 9, 3, 1}}, expect: 12},
+	}
+
+	//	开始测试
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			out := Rob2(c.input.nums)
+			t.Logf("success expect: %v, output: %v, with input: %+v", c.expect, out, c.input)
+		})
+	}
+}
+
+func TestRob3(t *testing.T) {
+	//	测试用例
+	cases := []*Case{
+		{name: "test 1", input: &Input{nums: []int{1, 2, 3, 1}}, expect: 4},
+		{name: "test 2", input: &Input{nums: []int{2, 7, 9, 3, 1}}, expect: 12},
+		{name: "test 3", input: &Input{nums: []int{1, 1}}, expect: 1},
+	}
+
+	//	开始测试
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			out := Rob3(c.input.nums)
+			t.Logf("success expect: %v, output: %v, with input: %+v", c.expect, out, c.input)
+		})
+	}
+}
