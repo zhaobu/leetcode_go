9579bbc3fc698b6be8bea0eb5ce6673de2e3d6b0
lwxm <lwxm>
2020-11-08 02:09:00 +0800

    完成496. 下一个更大元素 I

---
+++ src/0496.Next-Greater-Element-I/solution_test.go
@@ -0,0 +1,74 @@
+package Solution
+
+import (
+	"testing"
+)
+
+// func init() {
+// 	zaplog.InitLog(&zaplog.Config{
+// 		Logdir:   "./",
+// 		LogName:  "main.log",
+// 		Loglevel: "debug",
+// 		Debug:    true},
+// 	)
+// }
+
+//定义结构
+type Input struct {
+	nums1 []int
+	nums2 []int
+}
+
+type Case struct {
+	name   string
+	input  *Input
+	expect []int
+}
+
+func TestNextGreaterElement1(t *testing.T) {
+	//	测试用例
+	cases := []*Case{
+		{name: "test 1", input: &Input{nums1: []int{4, 1, 2}, nums2: []int{1, 3, 4, 2}}, expect: []int{-1, 3, -1}},
+		{name: "test 2", input: &Input{nums1: []int{2, 4}, nums2: []int{1, 2, 3, 4}}, expect: []int{3, -1}},
+	}
+
+	//	开始测试
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			out := nextGreaterElement1(c.input.nums1, c.input.nums2)
+			t.Logf("success expect: %v, output: %v, with input: %+v", c.expect, out, c.input)
+		})
+	}
+}
+
+func TestNextGreaterElement2(t *testing.T) {
+	//	测试用例
+	cases := []*Case{
+		{name: "test 1", input: &Input{nums1: []int{4, 1, 2}, nums2: []int{1, 3, 4, 2}}, expect: []int{-1, 3, -1}},
+		{name: "test 2", input: &Input{nums1: []int{2, 4}, nums2: []int{1, 2, 3, 4}}, expect: []int{3, -1}},
+	}
+
+	//	开始测试
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			out := nextGreaterElement2(c.input.nums1, c.input.nums2)
+			t.Logf("success expect: %v, output: %v, with input: %+v", c.expect, out, c.input)
+		})
+	}
+}
+
+func TestNextGreaterElement3(t *testing.T) {
+	//	测试用例
+	cases := []*Case{
+		{name: "test 1", input: &Input{nums1: []int{4, 1, 2}, nums2: []int{1, 3, 4, 2}}, expect: []int{-1, 3, -1}},
+		{name: "test 2", input: &Input{nums1: []int{2, 4}, nums2: []int{1, 2, 3, 4}}, expect: []int{3, -1}},
+	}
+
+	//	开始测试
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			out := nextGreaterElement3(c.input.nums1, c.input.nums2)
+			t.Logf("success expect: %v, output: %v, with input: %+v", c.expect, out, c.input)
+		})
+	}
+}
--- src/0503.Next-Greater-Element-II copy/solution_test.go
+++ src/0503.Next-Greater-Element-II copy/solution_test.go
@@ -15,28 +15,27 @@ import (
 
 //定义结构
 type Input struct {
-	prices []int
-	k      int
+	nums []int
 }
 
 type Case struct {
 	name   string
 	input  *Input
-	expect int
+	expect []int
 }
 
-func TestMaxProfit1(t *testing.T) {
+func TestNextGreaterElements1(t *testing.T) {
 	//	测试用例
 	cases := []*Case{
-		{name: "test 1", input: &Input{prices: []int{2, 4, 1}, k: 2}, expect: 2},
-		{name: "test 2", input: &Input{prices: []int{3, 2, 6, 5, 0, 3}, k: 2}, expect: 7},
+		{name: "test 1", input: &Input{nums: []int{1, 2, 1}}, expect: []int{2, -1, 2}},
+		{name: "test 2", input: &Input{nums: []int{2, 1, 2, 4, 3}}, expect: []int{4, 2, 4, -1, -1}},
 	}
 
 	//	开始测试
 	for _, c := range cases {
 		t.Run(c.name, func(t *testing.T) {
-			output := MaxProfit1(c.input.k, c.input.prices)
-			t.Logf("success expect: %v, output: %v, with input: %+v", c.expect, output, c.input)
+			out := nextGreaterElements1(c.input.nums)
+			t.Logf("success expect: %v, output: %v, with input: %+v", c.expect, out, c.input)
 		})
 	}
 }
