0898e50461218f30df3355bb0434ff05e0e1ea43
md_k8s_master <md_k8s_master>
2021-07-16 16:00:21 +0800

    844比较含退格的字符串

--- check/844.比较含退格的字符串.go
+++ check/844.比较含退格的字符串.go
@@ -6,7 +6,28 @@
 
 // @lc code=start
 func backspaceCompare(s string, t string) bool {
+	stackS := make([]rune, 0, len(s))
+	stackT := make([]rune, 0, len(t))
 
+	for _, v := range s {
+		if v != '#' {
+			stackS = append(stackS, v)
+		} else if len(stackS) > 0 {
+			stackS = stackS[0 : len(stackS)-1]
+		}
+	}
+
+	for _, v := range t {
+		if v != '#' {
+			stackT = append(stackT, v)
+		} else if len(stackT) > 0 {
+			stackT = stackT[0 : len(stackT)-1]
+		}
+	}
+
+	return string(stackS) == string(stackT)
 }
+
+//TODO双指针解法
 // @lc code=end
 
