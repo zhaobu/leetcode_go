180b3c424bae0ddd495e06f89f9f9bc9ccf3bf6b
vb-dev <zhaobulw@qq.com>
2021-12-17 13:09:20 +0000

    84未完待续

---
+++ check/84.柱状图中最大的矩形.go
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode.cn id=84 lang=golang
+ *
+ * [84] 柱状图中最大的矩形
+ */
+
+// @lc code=start
+func largestRectangleArea(heights []int) int {
+	if len(heights) < 1 {
+		return 0
+	}
+
+}
+
+// @lc code=end
+
