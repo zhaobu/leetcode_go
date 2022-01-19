ffce90da55fc3c45bfbf8a265d300e1062de0682
wsl-dev <zhaobulw@qq.com>
2022-01-19 15:58:01 +0800

    104.

---
+++ check/tree/104/104.二叉树的最大深度.go
@@ -0,0 +1,74 @@
+package main
+
+/*
+ * @lc app=leetcode.cn id=104 lang=golang
+ *
+ * [104] 二叉树的最大深度
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+/*
+解法1:深度优先,递归
+*/
+func maxDepth1(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	var (
+		max func(node *TreeNode) int
+	)
+	max = func(node *TreeNode) int {
+		if node == nil {
+			return 0
+		}
+		left := max(node.Left) + 1
+		right := max(node.Right) + 1
+		if left > right {
+			return left
+		}
+		return right
+	}
+
+	return max(root)
+}
+
+/*
+解法2:广度优先
+*/
+func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+
+	q := []*TreeNode{root}
+	height := 0
+	levelNums := 1
+
+	for len(q) > 0 {
+		head := q[0]
+		q = q[1:]
+		levelNums--
+		if head.Left != nil {
+			q = append(q, head.Left)
+		}
+		if head.Right != nil {
+			q = append(q, head.Right)
+		}
+		if levelNums == 0 {
+			height++
+			levelNums = len(q)
+		}
+	}
+
+	return height
+}
+
+// @lc code=end
---
+++ check/tree/104/104.二叉树的最大深度_test.go
@@ -0,0 +1,29 @@
+package main
+
+import "testing"
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func Test_maxDepth(t *testing.T) {
+	type args struct {
+		root *TreeNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := maxDepth(tt.args.root); got != tt.want {
+				t.Errorf("maxDepth() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
