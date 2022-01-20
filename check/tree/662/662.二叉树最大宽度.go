1a4fca7f3cf31683f666d4e5af60956274ad407b
wsl-dev <zhaobulw@qq.com>
2022-01-20 15:20:31 +0800

    662.

--- check/tree/662/662.二叉树最大宽度.go
+++ check/tree/662/662.二叉树最大宽度.go
@@ -20,16 +20,34 @@ func widthOfBinaryTree(root *TreeNode) int {
 		return 0
 	}
 	var (
-		curLevel  = []*TreeeNode{root} //当前层的所有节点
-		levelNums = 1
-		nextLevel []*TreeNode //下一层的所有节点
+		curLevel  = []*TreeNode{root} //当前层的所有节点
+		nextLevel []*TreeNode         //下一层的所有节点
+		maxWidth  = 1                 //最大宽度
+		max       func(a, b int) int
 	)
-	root.Val = 0
+	max = func(a, b int) int {
+		if a > b {
+			return a
+		}
+		return b
+	}
+	root.Val = 0 //用节点的值来保存index
 	for len(curLevel) > 0 {
 		//遍历当前层的所有节点
-		nextLevel = make([]*TreeeNode, 0, len(curLevel))
+		nextLevel = make([]*TreeNode, 0, 2*len(curLevel))
+		//访问当前层所有节点
+		maxWidth = max(maxWidth, curLevel[len(curLevel)-1].Val-curLevel[0].Val+1)
 		for i := 0; i < len(curLevel); i++ {
-			if curLevel[i] 
+			if curLevel[i].Left != nil {
+				curLevel[i].Left.Val = 2*curLevel[i].Val + 1
+				nextLevel = append(nextLevel, curLevel[i].Left)
+			}
+			if curLevel[i].Right != nil {
+				curLevel[i].Right.Val = 2*curLevel[i].Val + 2
+				nextLevel = append(nextLevel, curLevel[i].Right)
+			}
 		}
+		curLevel = nextLevel
 	}
+	return maxWidth
 }
