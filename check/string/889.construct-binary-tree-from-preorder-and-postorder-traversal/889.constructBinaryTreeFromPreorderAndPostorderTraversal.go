6625ca26a97bba51260421496b87d2f2a608c205
lwxmwsl <zhaobulw@qq.com>
2022-01-24 00:50:25 +0800

    提交889 题目

---
+++ check/string/889.construct-binary-tree-from-preorder-and-postorder-traversal/889.constructBinaryTreeFromPreorderAndPostorderTraversal.go
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode.cn id=889 lang=golang
+ *
+ * [889] Construct Binary Tree from Preorder and Postorder Traversal
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func constructFromPrePost(preorder []int, postorder []int) *TreeNode {
+
+}
+// @lc code=end
+
