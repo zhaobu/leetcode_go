0fbc00d5eb8b465a4cf0bdf1ff7c0f2132aeb7b4
lwxmwsl <zhaobulw@qq.com>
2022-02-25 00:39:23 +0800

    1

---
+++ check/string/151/151.翻转字符串里的单词.go
@@ -0,0 +1,54 @@
+package main
+
+import "fmt"
+
+/*
+ * @lc app=leetcode.cn id=151 lang=golang
+ *
+ * [151] 翻转字符串里的单词
+ */
+
+// @lc code=start
+func reverseWords(s string) string {
+	//去除多余的空格
+	b := make([]byte, 0, len(s))
+	preSpace := true  //前一个字符是否空格
+	bWord := []byte{} //待加入的新的单词
+	for i := 0; i < len(s); i++ {
+		if s[i] != ' ' {
+			b = append(b, s[i])
+			preSpace = false
+		} else if !preSpace {
+			if i < len(s)-1 {
+				b = append(b, ' ')
+			}
+			preSpace = true
+		}
+	}
+	fmt.Printf("string(b)=[%s]\n", string(b))
+	// 翻转字符串
+	var swapStr func(b []byte, left, right int)
+	swapStr = func(b []byte, left, right int) {
+		for i, j := left, right; i < j; i, j = i+1, j-1 {
+			b[i], b[j] = b[j], b[i]
+		}
+		return
+	}
+
+	swapStr(b, 0, len(b)-1)
+
+	fmt.Printf("string(b)=[%s]\n", string(b))
+
+	begin := 0
+	for i := 0; i < len(b); i++ {
+		if b[i] == ' ' {
+			swapStr(b, begin, i-1)
+			begin = i + 1
+		}
+	}
+	//翻转最后一个单词
+	swapStr(b, begin, len(b)-1)
+	return string(b)
+}
+
+// @lc code=end
---
+++ check/string/151/151.翻转字符串里的单词_test.go
@@ -0,0 +1,30 @@
+package main
+
+import "testing"
+
+func Test_reverseWords(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		// TODO: Add test cases.
+		{
+			name: "1",
+			args: args{
+				s: "  hello world  ",
+			},
+			want: "world hello",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := reverseWords(tt.args.s); got != tt.want {
+				t.Errorf("reverseWords() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
