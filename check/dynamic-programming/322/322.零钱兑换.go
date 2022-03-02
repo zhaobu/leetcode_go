7754f24d27ef4e3ee854ce6392ddfef69e0c337c
lwxmwsl <zhaobulw@qq.com>
2022-03-02 21:55:22 +0800

    [322] 零钱兑换 递归解法

--- check/dynamic-programming/322/322.零钱兑换.go
+++ check/dynamic-programming/322/322.零钱兑换.go
@@ -7,7 +7,21 @@ package main
  */
 
 // @lc code=start
-func coinChange(coins []int, amount int) int {
+
+/*
+解法1: 动态规划
+状态定义:
+dp[i] 表示凑齐金额 i 所需的最少硬币总数
+
+状态初始值
+dp[0]=0 表示凑够金额0需要0个硬币
+
+转移方程
+如果当前硬币面值为 coin, 如果用到该种硬币,则所需最少硬币为
+dp[i]=dp[i-coin]+1
+用1枚当前硬币后,剩余需要凑够的钱为 i-coin
+*/
+func coinChange1(coins []int, amount int) int {
 	if len(coins) < 1 {
 		return -1
 	}
@@ -39,6 +53,52 @@ func coinChange(coins []int, amount int) int {
 		}
 	}
 	return dp[amount]
+}
+
+/*
+解法2: 暴力递归
+超时,只为拓展解题思路
+此外还可以用利用hashMap记忆
+*/
+func coinChange(coins []int, amount int) int {
+	if len(coins) < 1 {
+		return -1
+	}
+	if amount < 1 {
+		return 0
+	}
+
+	//凑够leftAmount所需的最少硬币总数
+	var change func(leftAmount int) int
+	change = func(leftAmount int) int {
+		if leftAmount < 0 {
+			return -1
+		}
+		if leftAmount == 0 {
+			return 0
+		}
+		for _, v := range coins {
+			if v == leftAmount {
+				return 1
+			}
+		}
+
+		min := -1 //初始化为无法凑出
+		for _, v := range coins {
+			cur := change(leftAmount - v)
+			if cur == -1 {
+				//如果用到该种面值的硬币会导致无法凑出,直接跳过
+				continue
+			}
+			//初次凑出或者遇到更优解就更新最优解
+			if cur+1 < min || min == -1 {
+				min = cur + 1
+			}
+		}
+		return min
+	}
+
+	return change(amount)
 }
 
 // @lc code=end
