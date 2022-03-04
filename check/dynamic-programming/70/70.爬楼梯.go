4a86a5c1f08959dbdcaed8d91e57b6a80ab286cc
lwxmwsl <zhaobulw@qq.com>
2022-03-04 21:16:14 +0800

    [70] 爬楼梯

--- check/dynamic-programming/70/70.爬楼梯.go
+++ check/dynamic-programming/70/70.爬楼梯.go
@@ -7,22 +7,44 @@ package main
  */
 
 // @lc code=start
-func climbStairs(n int) int {
-	if n < 1 {
-		return 0
-	}
-	if n == 1 || n == 2 {
+
+/*
+解法1 动态规划
+dp[i] = dp[i-1] + dp[i-2]
+*/
+func climbStairs1(n int) int {
+	if n < 3 {
 		return n
 	}
-	f2 := 1 // 1个台阶
-	f1 := 2 //2个台阶
-	fn := 0 //n级台阶
-	for i := 3; i <= n; i++ {
-		fn = f1 + f2 //f(n)=f(n-1)+f(n-2)
-		f2 = f1      //更新f(n-2)的值为f(n-1)
-		f1 = fn      //更新f(n-1)为最新的fn
+	//dp[i]表示爬到i+1阶楼梯的方法总数
+	dp := make([]int, n)
+	dp[0] = 1
+	dp[1] = 2
+	for i := 2; i < n; i++ {
+		dp[i] = dp[i-1] + dp[i-2]
 	}
-	return fn
+
+	return dp[n-1]
+}
+
+/*
+解法2 动态规划 优化空间复杂度
+因为 dp[i] = dp[i-1] + dp[i-2]
+所以可以只用3个变量来保存
+*/
+func climbStairs(n int) int {
+	if n < 3 {
+		return n
+	}
+	pre2 := 1 // 1个台阶
+	pre1 := 2 //2个台阶
+	cur := 0  //n级台阶
+	for i := 3; i <= n; i++ {
+		cur = pre1 + pre2 //dp[i] = dp[i-1] + dp[i-2]
+		pre2 = pre1       //dp[i-2]的值为dp[i-1]
+		pre1 = cur        //更新dp[i-1]的值为dp[i]
+	}
+	return cur
 }
 
 // @lc code=end
