284c00c2e54c2f7569587d684e33d1354454c061
lwxmwsl <zhaobulw@qq.com>
2022-03-09 20:52:00 +0800

    [1235] 规划兼职工作 二分搜索优化查找

--- check/Unknown/1235/1235.规划兼职工作.go
+++ check/Unknown/1235/1235.规划兼职工作.go
@@ -12,7 +12,7 @@ import "sort"
 /*
 解法1 排序+动态规划
 */
-func jobScheduling(startTime []int, endTime []int, profit []int) int {
+func jobScheduling1(startTime []int, endTime []int, profit []int) int {
 	type job struct {
 		startTime int
 		endTime   int
@@ -52,6 +52,72 @@ func jobScheduling(startTime []int, endTime []int, profit []int) int {
 				break
 			}
 		}
+		//从不做第i件工作和做第i件工作中找到最大值
+		dp[i] = max(dp[i-1], dp[pre]+jobs[i].profit)
+	}
+	return dp[len(jobs)-1]
+}
+
+/*
+解法2 解法1的优化,二分查找前一份工作
+*/
+func jobScheduling(startTime []int, endTime []int, profit []int) int {
+	type job struct {
+		startTime int
+		endTime   int
+		profit    int
+	}
+	jobs := make([]*job, 0, len(startTime))
+	for i, _ := range startTime {
+		jobs = append(jobs, &job{
+			startTime: startTime[i],
+			endTime:   endTime[i],
+			profit:    profit[i],
+		})
+	}
+
+	sort.Slice(jobs, func(i, j int) bool { return jobs[i].endTime <= jobs[j].endTime })
+
+	//dp[i] 表示第i件工作结束后获得的最大报酬
+	dp := make([]int, len(jobs))
+	dp[0] = jobs[0].profit
+	var max = func(a, b int) int {
+		if a > b {
+			return a
+		}
+		return b
+	}
+
+	//二分查找最后一个不大于target的元素
+	var bSearch = func(i, j, target int) int {
+
+		for i <= j {
+			mid := i + (j-i)>>1
+			if jobs[mid].endTime > target {
+				j = mid - 1
+			} else {
+				if mid == j || jobs[mid+1].endTime > target {
+					return mid
+				}
+				i = mid + 1
+			}
+		}
+
+		return -1
+	}
+
+	for i := 1; i < len(jobs); i++ {
+		/*
+			1. 从i-1开始到0 逆序找到endtime不晚于jobs[i]的starttime的第一份工作
+			2. 如果找不到前一份工作,但jobs[i]又必须做,则赚到的钱就是jobs[i].profit
+			3. pre默认初始化为i,这样可以保证找不到时dp[pre]=0.
+		*/
+		pre := i
+		k := bSearch(0, i-1, jobs[i].startTime)
+		if k != -1 {
+			pre = k
+		}
+
 		//从不做第i件工作和做第i件工作中找到最大值
 		dp[i] = max(dp[i-1], dp[pre]+jobs[i].profit)
 	}
--- check/Unknown/1235/1235.规划兼职工作_test.go
+++ check/Unknown/1235/1235.规划兼职工作_test.go
@@ -13,33 +13,33 @@ func Test_jobScheduling(t *testing.T) {
 		args args
 		want int
 	}{
-		// {
-		// 	name: "test1",
-		// 	args: args{
-		// 		startTime: []int{1, 2, 3, 3},
-		// 		endTime:   []int{3, 4, 5, 6},
-		// 		profit:    []int{50, 10, 40, 70},
-		// 	},
-		// 	want: 120,
-		// },
-		// {
-		// 	name: "test2",
-		// 	args: args{
-		// 		startTime: []int{1, 2, 3, 4, 6},
-		// 		endTime:   []int{3, 5, 10, 6, 9},
-		// 		profit:    []int{20, 20, 100, 70, 60},
-		// 	},
-		// 	want: 150,
-		// },
 		{
-			name: "test3",
+			name: "test1",
 			args: args{
-				startTime: []int{1, 1, 1},
-				endTime:   []int{2, 3, 4},
-				profit:    []int{5, 6, 4},
+				startTime: []int{1, 2, 3, 3},
+				endTime:   []int{3, 4, 5, 6},
+				profit:    []int{50, 10, 40, 70},
 			},
-			want: 6,
+			want: 120,
 		},
+		{
+			name: "test2",
+			args: args{
+				startTime: []int{1, 2, 3, 4, 6},
+				endTime:   []int{3, 5, 10, 6, 9},
+				profit:    []int{20, 20, 100, 70, 60},
+			},
+			want: 150,
+		},
+		// {
+		// 	name: "test3",
+		// 	args: args{
+		// 		startTime: []int{1, 1, 1},
+		// 		endTime:   []int{2, 3, 4},
+		// 		profit:    []int{5, 6, 4},
+		// 	},
+		// 	want: 6,
+		// },
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
