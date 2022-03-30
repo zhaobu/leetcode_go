6618e47402bde53b5aaf9363751b49bbdb9f48b3
lwxmwsl <zhaobulw@qq.com>
2022-03-31 01:09:53 +0800

    [8] 字符串转换整数 (atoi) 状态机解法

--- check/string/8/8.字符串转换整数Atoi.go
+++ check/string/8/8.字符串转换整数Atoi.go
@@ -9,26 +9,26 @@ package main
 // @lc code=start
 
 /*
-解法1
+解法1 直接转换
 */
-func myAtoi(s string) int {
+func myAtoi1(s string) int {
 	ret := 0
 	const (
 		Positive = 1
 		Nagivate = -1
 	)
-	symbol := Positive
+	sign := Positive
 	//处理符号部分
 	i := 0
 	for ; i < len(s); i++ {
 		if s[i] == ' ' {
 			continue
 		} else if s[i] == '-' {
-			symbol = Nagivate
+			sign = Nagivate
 			i++
 			break
 		} else if s[i] == '+' {
-			symbol = Positive
+			sign = Positive
 			i++
 			break
 		} else {
@@ -40,7 +40,6 @@ func myAtoi(s string) int {
 		Max = 1<<31 - 1
 		Min = -1 << 31
 	)
-
 	//处理数字部分
 	for ; i < len(s); i++ {
 		if s[i] >= '0' && s[i] <= '9' {
@@ -52,10 +51,72 @@ func myAtoi(s string) int {
 				ret = Min
 				break
 			}
-			ret = ret*10 + symbol*num
+			ret = ret*10 + sign*num
 		} else {
 			break
 		}
+	}
+
+	return ret
+}
+
+/*
+解法2 状态机
+*/
+func myAtoi(s string) int {
+	ret := 0
+	const (
+		Positive = 1
+		Nagivate = -1
+	)
+	const (
+		Start    = 0
+		Signed   = 1
+		InNumber = 2
+		End      = 3
+	)
+	stateTable := [][]int{
+		{Start, Signed, InNumber, End},
+		{End, End, InNumber, End},
+		{End, End, InNumber, End},
+		{End, End, End, End},
+	}
+
+	sign := Positive
+	state := Start
+	getCharIndex := func(char rune) int {
+		if char == ' ' {
+			return 0
+		} else if char == '+' || char == '-' {
+			return 1
+		} else if char >= '0' && char <= '9' {
+			return 2
+		}
+		return 3
+	}
+	const (
+		Max = 1<<31 - 1
+		Min = -1 << 31
+	)
+
+	automaton := func(char rune) {
+		state = stateTable[state][getCharIndex(char)]
+		if state == InNumber {
+			num := int(char - '0')
+			ret = ret*10 + sign*num
+			if ret > Max {
+				ret = Max
+			} else if ret < Min {
+				ret = Min
+			}
+		} else if state == Signed {
+			if char == '-' {
+				sign = Nagivate
+			}
+		}
+	}
+	for _, v := range s {
+		automaton(v)
 	}
 
 	return ret
