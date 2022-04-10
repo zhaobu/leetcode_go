dbdfcb868b1d05138af94919767234f09eb1e506
lwxmwsl <zhaobulw@qq.com>
2022-04-10 19:24:51 +0800

    [203] 移除链表元素

---
+++ check/linked-list/203/203.移除链表元素.go
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode.cn id=203 lang=golang
+ *
+ * [203] 移除链表元素
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+/*
+ 解法1: 利用辅助结点
+*/
+func removeElements(head *ListNode, val int) *ListNode {
+	if head == nil {
+		return nil
+	}
+
+	help := &ListNode{Next: head} //辅助节点
+	cur, pre := head, help
+	for cur != nil {
+		if cur.Val == val {
+			pre.Next = cur.Next
+		} else {
+			pre = cur
+		}
+		cur = cur.Next
+	}
+	return help.Next
+}
+
+// @lc code=end
+
