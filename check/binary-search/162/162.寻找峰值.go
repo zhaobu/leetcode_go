07480003f745f21b67e9857401a3863559c4fca3
lwxmwsl <zhaobulw@qq.com>
2022-04-20 11:50:33 +0800

    [162] 寻找峰值

--- check/binary-search/162/162.寻找峰值.go
+++ check/binary-search/162/162.寻找峰值.go
@@ -12,7 +12,7 @@ package main
 解法1 直接求得第一个波峰
 求第一个nums[i] > nums[i+1]的值,所以在区间[0,i]上必然是递增的
 */
-func findPeakElement(nums []int) int {
+func findPeakElement1(nums []int) int {
 	n := len(nums)
 	if n < 2 { //n必定>=1
 		return 0
@@ -24,6 +24,41 @@ func findPeakElement(nums []int) int {
 		}
 	}
 	return n - 1
+}
+
+/*
+解法2 二分查找
+*/
+func findPeakElement(nums []int) int {
+	n := len(nums)
+	if n < 2 { //n必定>=1
+		return 0
+	}
+
+	//判断nums[idx1] < nums[idx2]
+	less := func(idx1, idx2 int) bool {
+		if idx1 == -1 || idx1 == n {
+			return true
+		}
+		if idx2 == n || idx2 == -1 {
+			return false
+		}
+		return nums[idx1] < nums[idx2]
+	}
+
+	left, right := 0, n-1
+	for left <= right {
+		mid := left + (right-left)>>1
+		if less(mid-1, mid) && less(mid+1, mid) {
+			return mid
+		}
+		if less(mid, mid+1) {
+			left = mid + 1
+		} else {
+			right = mid - 1
+		}
+	}
+	return -1
 }
 
 // @lc code=end
---
+++ check/binary-search/162/162.寻找峰值_test.go
@@ -0,0 +1,32 @@
+package main
+
+import "testing"
+
+func Test_findPeakElement(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "test1",
+			args: args{nums: []int{1, 2}},
+			want: 1,
+		},
+		{
+			name: "test2",
+			args: args{nums: []int{1, 2, 3, 5, 4, 6}},
+			want: 3,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := findPeakElement(tt.args.nums); got != tt.want {
+				t.Errorf("findPeakElement() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
