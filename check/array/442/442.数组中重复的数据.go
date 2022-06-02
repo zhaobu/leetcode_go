6434d51518860d170aa7600315cadfe12d4a7e3a
lwxmwsl <zhaobulw@qq.com>
2022-06-02 16:33:23 +0800

    [442] 数组中重复的数据

--- check/array/442/442.数组中重复的数据.go
+++ check/array/442/442.数组中重复的数据.go
@@ -1,3 +1,5 @@
+package main
+
 /*
  * @lc app=leetcode.cn id=442 lang=golang
  *
@@ -5,8 +7,27 @@
  */
 
 // @lc code=start
+
+/*
+解法1 原地交换
+*/
 func findDuplicates(nums []int) []int {
+	ret := []int{}
 
+	//把元素交换到正确的位置
+	for i := range nums {
+		//把nums[i]放到争取的位置上
+		for nums[i] != nums[nums[i]-1] {
+			nums[i], nums[nums[i]-1] = nums[nums[i]-1], nums[i]
+		}
+	}
+	for i := range nums {
+		if nums[i] != i+1 {
+			ret = append(ret, nums[i])
+		}
+	}
+	// fmt.Printf("nums=%+v\n", nums)
+	return ret
 }
-// @lc code=end
 
+// @lc code=end
---
+++ check/array/442/442.数组中重复的数据_test.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_findDuplicates(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		// {
+		// 	name: "test1",
+		// 	args: args{
+		// 		nums: []int{5, 4, 6, 7, 9, 3, 10, 9, 5, 6},
+		// 	},
+		// 	want: []int{9, 6, 5},
+		// },
+		// {
+		// 	name: "test2",
+		// 	args: args{
+		// 		nums: []int{1, 1, 2},
+		// 	},
+		// 	want: []int{1},
+		// },
+		{
+			name: "test3",
+			args: args{
+				nums: []int{10, 2, 5, 10, 9, 1, 1, 4, 3, 7},
+			},
+			want: []int{10, 1},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := findDuplicates(tt.args.nums); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("findDuplicates() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
